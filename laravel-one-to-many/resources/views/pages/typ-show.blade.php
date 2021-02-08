66ee6be11fd0144fea9113621cb54b1d7fe26ed6
sa-borgia3 <sa.borgia3@gmail.com>
2021-02-08 15:58:49 +0100

    name and description label for typologies show

--- laravel-one-to-many/resources/views/pages/typ-show.blade.php
+++ laravel-one-to-many/resources/views/pages/typ-show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main-layout')
 @section('content')
 
-    <h1>TIPOLOGIES</h1>
+    <h1>TIPOLOGY</h1>
 
-    <h3>{{ $typology->name }}</h3>
-    <h3>{{ $typology->description }}</h3>
+    <h3>Name: {{ $typology->name }}</h3>
+    <h3>Description: {{ $typology->description }}</h3>
     <br>
     <h1>Associated tasks:</h1>
     <ul>
